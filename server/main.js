9d16a66aa059eeb246e59f4b1bc1ed56dba02447
DFMC017 <douglas.mendez@fundacion-jala.org>
2023-05-19 12:50:09 -0400

    Proyect Presentation

--- server/main.js
+++ server/main.js
@@ -3,6 +3,7 @@ const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
+
 app.use(bodyParser.json());
 
 app.use(cors({
@@ -11,20 +12,19 @@ app.use(cors({
 
 const port = 3000;
 
-
 app.get('/', (req, res) => {
   res.send('API is working');
 })
 
 app.post('/driver',(req, res) => {
   const body = req.body;
+
   const query = `INSERT INTO user(firstName, lastName, email) VALUES ('${body.firstName}', '${body.lastName}','${body.email}');`;
-  connection.connect();
   connection.query( query, (err, rows, fields) => {
     if (err) throw err
     console.log('1 record inserted');
   })
-  connection.end();
+
   res.send('1 record inserted');
 })
 
@@ -34,8 +34,30 @@ app.listen(port, () => {
 
 const connection = mysql.createConnection({
   host: 'localhost',
-  port: '9090',
+  port: '3307',
   user: 'root',
-  password: 'root2',
-  database: 'In Drive'
+  password: 'root',
+  database: 'db1'
 })
+
+connection.connect();
+
+/*var mysql      = require('mysql2');
+var connection = mysql.createConnection({
+  host: "localhost",
+    user: "new_user",
+    password: "password",
+    database: "db1",
+    port:"3307",
+    multipleStatements: true
+});
+connection.connect();
+
+connection.query('SELECT * FROM user', function(err, rows, fields) 
+{
+  if (err) throw err;
+
+  console.log(rows[0]);
+});
+
+connection.end();*/
